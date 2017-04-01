1f183fdb08913c95cf85857a598fc16fd3a7ee08
root <root@localhost>
2017-04-01 13:28:18 +0000

    change url from /ccc to /rim_learning_spring_mvc

--- Rim_Learning(SPRING-MVC)/src/main/java/com/art/consulting/dao/ConferenceRepository.java
+++ Rim_Learning(SPRING-MVC)/src/main/java/com/art/consulting/dao/ConferenceRepository.java
@@ -3,15 +3,13 @@ package com.art.consulting.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.art.consulting.entities.ConferenceTable;
 
 
 public interface ConferenceRepository extends JpaRepository<ConferenceTable, Integer> {
 
-	
-	
-	
 	 
 	 
 }
